6573465f602048c687793ae91f6ddff107752170
caro.straub <carolina.straub@gmail.com>
2025-08-16 12:32:16 -0400

    nuevos ejercicios

---
+++ Access_and_retrieve.py
@@ -0,0 +1,7 @@
+myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+# 1. print the 3rd item here
+print(myArray[2])
+#2. change the 'thursday' value to null here
+myArray[4]= None
+#3. print the position of step 2
+print(myArray[4])
---
+++ Print_the_last_one.py
@@ -0,0 +1,15 @@
+import random
+""" Create a variable named theLastOne, and assign to it the LAST element of myStupidArray.
+
+Then, print it on the console. """
+def generate_random_array():
+    aux_Array =[]
+    random_length = random.randint(0, 99)
+    for i in range(random_length):
+        aux_Array.append(random.randint(0, 99))
+    return aux_Array
+
+my_stupid_array = generate_random_array()
+
+the_last_one = my_stupid_array[-1]
+print(the_last_one)
