cfd660a8fed727ff01c5ec5603d07b04dc17e5a7
caro.straub <carolina.straub@gmail.com>
2025-08-18 16:04:41 -0400

    más ejercicios

--- Add_items_and_loop.py
+++ Add_items_and_loop.py
@@ -1,2 +1,7 @@
-#Add 10 random integers to the arr list, in every loop add one of the new random integers and print the array on the console.
+#Add 10 random integers to the arr list and print the array on the console.
+
+import random
 arr = [4,5,734,43,45];
+for i in range(10):
+    arr.append(random.randint(0,99))
+print(arr)
